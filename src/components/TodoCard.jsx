f0fcd74285ff720b254c36130333ccf44f2aa7f2
Saurabh <devsaurabh086@gmail.com>
2025-03-18 23:47:03 +0530

    new changes

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -1,14 +1,11 @@
 import { useState } from "react";
 
 export default function TodoCard({ todo, handleDeleteTodo, todoIndex, handleCompleteTodo, handleEditTodo }) {
-
-  const [isEditing, setIsEditing] = useState(false); //isEditing is defaultly set to false! (Sequence-1)
+  const [isEditing, setIsEditing] = useState(false);
   const [newInput, setNewInput] = useState(todo.input);
-
 
   return (
     <div className="card todo-item"> 
-      {/* Sequence-3 , When the page re-render after the click of edit btn, a new input box gets rendered instead of todo.input since isEditing is set to true! */}
       {isEditing ? (
         <input type="text" value={newInput} onChange={(e) => setNewInput(e.target.value)} /> 
       ) : (
@@ -17,14 +14,21 @@ export default function TodoCard({ todo, handleDeleteTodo, todoIndex, handleComp
 
       <div className="todo-buttons">
         {isEditing ? (
-          <button onClick={()=>{
-            handleEditTodo(todoIndex , newInput)
+          <button onClick={() => {
+            handleEditTodo(todoIndex, newInput);
             setIsEditing(false);
-          }}><h6>Save</h6></button> 
+          }}>
+            <h6>Save</h6>
+          </button> 
         ) : (
           <>
-            <button onClick={() => setIsEditing(true)}><h6>Edit</h6></button> {/* (Sequence-2) When the edit btn is clicked , isEditing is set to true and the page re-renders itself*/}
-            <button onClick={() => handleCompleteTodo(todoIndex)} disabled={todo.complete}><h6>Done</h6></button>
+            <button onClick={() => setIsEditing(true)}><h6>Edit</h6></button>
+            <button 
+              onClick={() => handleCompleteTodo(todoIndex)} 
+              className={todo.complete ? "completed" : ""}
+            >
+              <h6>Done</h6>
+            </button>
             <button onClick={() => handleDeleteTodo(todoIndex)}><h6>Delete</h6></button>
           </>
         )}
